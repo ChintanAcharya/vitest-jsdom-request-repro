2eb90e21ac3fa69ee59822ba49778c2c21667597
Chintan <contact@chintanacharya.dev>
2025-08-01 20:58:01 +0530

    Add fetch example

--- request.test.ts
+++ request.test.ts
@@ -1,6 +1,15 @@
-import {expect, test} from 'vitest';
+import { expect, test } from "vitest";
 
-test('does not crash when creating a Request with AbortSignal', () => {
-  const request = new Request('https://example.com', {signal: new AbortController().signal});
+test("does not crash when using fetch with AbortSignal", async () => {
+  const response = await fetch("https://example.com", {
+    signal: new AbortController().signal,
+  });
+  expect(response).toBeInstanceOf(Response);
+});
+
+test("does not crash when creating a Request with AbortSignal", () => {
+  const request = new Request("https://example.com", {
+    signal: new AbortController().signal,
+  });
   expect(request).toBeInstanceOf(Request);
 });
